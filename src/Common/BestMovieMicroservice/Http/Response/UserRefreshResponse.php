d5b8b544c10eb2e93dd5603cf75f6ab8de6b8cb8
Alexxx598 <aleksspesivtsev@gmail.com>
2023-02-24 16:15:50 +0200

    feature added isAdmin check

--- src/Common/BestMovieMicroservice/Http/Response/UserRefreshResponse.php
+++ src/Common/BestMovieMicroservice/Http/Response/UserRefreshResponse.php
@@ -6,5 +6,15 @@ use BestMovie\Common\BaseApi\BaseResponse\BaseResponse;
 
 class UserRefreshResponse extends BaseResponse
 {
+    protected const ADMIN = 'ADMIN';
 
+    /**
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return collect($this->getData()['roles'] ?? [])
+            ->filter(fn (string $role) => $role === self::ADMIN)
+            ->isNotEmpty();
+    }
 }
